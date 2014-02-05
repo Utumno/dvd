962472c044e73c8ce6be34160d33d894a6e8b714
MrD <the.ubik@gmail.com>
2014-02-05 12:24:48 +0000

    Formatting of user class

--- src/dvd_store/entities/User.java
+++ src/dvd_store/entities/User.java
@@ -1,24 +1,33 @@
 package dvd_store.entities;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
-/**
- * The persistent class for the users database table.
- * 
- */
+/** The persistent class for the users database table. */
 @Entity
-@Table(name="users")
-@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+@Table(name = "users")
+@NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	private int iduser;
-
 	@Temporal(TemporalType.DATE)
 	@Past(message = "Back from the future ?")
 	@NotNull(message = "Please enter your date of birth")
@@ -40,39 +49,23 @@ public class User implements Serializable {
 	private String surname;
 	@NotNull(message = "Please enter a username")
 	private String username;
-
-	//uni-directional many-to-many association to Address
+	// uni-directional many-to-many association to Address
 	@ManyToMany
-	@JoinTable(
-		name="users_has_addresses"
-		, joinColumns={
-			@JoinColumn(name="users_iduser")
-			}
-		, inverseJoinColumns={
-			@JoinColumn(name="addresses_idaddress")
-			}
-		)
+	@JoinTable(name = "users_has_addresses", joinColumns = { @JoinColumn(
+			name = "users_iduser") }, inverseJoinColumns = { @JoinColumn(
+			name = "addresses_idaddress") })
 	private List<Address> addresses;
-
-	//uni-directional many-to-many association to CreditCard
+	// uni-directional many-to-many association to CreditCard
 	@ManyToMany
-	@JoinTable(
-		name="users_has_credit_cards"
-		, joinColumns={
-			@JoinColumn(name="users_iduser")
-			}
-		, inverseJoinColumns={
-			@JoinColumn(name="credit_cards_credit_card_number")
-			}
-		)
+	@JoinTable(name = "users_has_credit_cards", joinColumns = { @JoinColumn(
+			name = "users_iduser") }, inverseJoinColumns = { @JoinColumn(
+			name = "credit_cards_credit_card_number") })
 	private List<CreditCard> creditCards;
-
-	//bi-directional many-to-one association to Order
-	@OneToMany(mappedBy="user")
+	// bi-directional many-to-one association to Order
+	@OneToMany(mappedBy = "user")
 	private List<Order> orders;
 
-	public User() {
-	}
+	public User() {}
 
 	public int getIduser() {
 		return this.iduser;
@@ -165,15 +158,12 @@ public class User implements Serializable {
 	public Order addOrder(Order order) {
 		getOrders().add(order);
 		order.setUser(this);
-
 		return order;
 	}
 
 	public Order removeOrder(Order order) {
 		getOrders().remove(order);
 		order.setUser(null);
-
 		return order;
 	}
-
 }
